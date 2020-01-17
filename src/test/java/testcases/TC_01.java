c4ffd8f1c0dcf72de82c503f75b3e08f20dd97e5
avinash kumar <avinashkumar2021@gmail.com>
2020-01-17 09:10:26 +0530

    Create TC_01.java

---
+++ src/test/java/testcases/TC_01.java
@@ -0,0 +1,63 @@
+package testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pages.BankPassword;
+import pages.ConfirmationMessage;
+import pages.Homepage;
+import pages.OrderSummaryCart;
+import pages.ShoppingCart;
+import utility.ExcelUtils;
+
+public class TC_01 extends BaseClass{
+	
+	
+	
+	
+	
+	@Test(dataProvider = "TestData")
+	public void validateSuccessfullTransaction(String Name,String Email,String PhoneNo,String City,String PostalCode,String CardNumber,String ExpiryDate,String cvv,String Otp) throws Exception {
+		logger=extent.startTest(ExcelUtils.getTestCaseName(this.toString()));
+		
+		
+		
+		
+		Homepage.using(driver)
+		        .ClickBuyNow();
+		
+		ShoppingCart.using(driver)
+		            .setName(Name)
+		            .setEmail(Email)
+		            .setPhoneNo(PhoneNo)
+		            .setCity(City)
+		            .setPostalCode(PostalCode)
+		            .clickCheckOut();
+		
+		
+		OrderSummaryCart.using(driver)
+		                .clickContinue()
+		                .clickOnPayment()
+		                .setCardNumber(CardNumber)
+		                .setExpiryDate(ExpiryDate)
+		                .setCVV(cvv)
+		                .submitPayment();
+		    BankPassword.using(driver)
+		                .setBankOtp(Otp)
+		                .submitOtp();
+		    
+		   String Message= ConfirmationMessage.using(driver)
+		                       .getTransactionMessage();
+		
+		                
+		   Assert.assertEquals(Message, "Transaction successful");
+		
+		
+		System.out.println(Message);
+		 extent.endTest(logger);               
+		
+		
+	}
+	
+	
+}
